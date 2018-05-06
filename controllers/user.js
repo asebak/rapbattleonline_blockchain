24aa4e6b7978c10e064258273a6dcd3f36f6883c
Ahmad <radio_frequency@hotmail.com>
2018-05-06 09:00:12 -0400

    added registration ability

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const passport = require('passport');
 const User = require('../models/User');
+const Nas = require('nebulas');
 
 const randomBytesAsync = promisify(crypto.randomBytes);
 
@@ -80,10 +81,7 @@ exports.getSignup = (req, res) => {
  * Create a new local account.
  */
 exports.postSignup = (req, res, next) => {
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('password', 'Password must be at least 4 characters long').len(4);
   req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-  req.sanitize('email').normalizeEmail({ gmail_remove_dots: false });
 
   const errors = req.validationErrors();
 
@@ -91,28 +89,13 @@ exports.postSignup = (req, res, next) => {
     req.flash('errors', errors);
     return res.redirect('/signup');
   }
-
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  });
-
-  User.findOne({ email: req.body.email }, (err, existingUser) => {
-    if (err) { return next(err); }
-    if (existingUser) {
-      req.flash('errors', { msg: 'Account with that email address already exists.' });
-      return res.redirect('/signup');
-    }
-    user.save((err) => {
-      if (err) { return next(err); }
-      req.logIn(user, (err) => {
-        if (err) {
-          return next(err);
-        }
-        res.redirect('/');
-      });
-    });
-  });
+  var account = Nas.Account.NewAccount();
+  var address = account.getAddressString();
+  var keyStr = account.toKeyString(req.body.password);
+  res.render('account/signup', { 
+    address: address,
+    key: keyStr
+   });
 };
 
 /**
